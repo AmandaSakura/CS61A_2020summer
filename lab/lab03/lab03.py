8f8964f3c3857b7052c3e2f96b6e5479b347b980
AmandaSakura <1298018274@qq.com>
2024-03-22 10:55:42 +0800

    update

---
+++ lab/lab03/lab03.py
@@ -0,0 +1,157 @@
+HW_SOURCE_FILE=__file__
+
+
+def pascal(row, column):
+    """Returns a number corresponding to the value at that location
+    in Pascal's Triangle.
+    >>> pascal(0, 0)
+    1
+    >>> pascal(0, 5)	# Empty entry; outside of Pascal's Triangle
+    0
+    >>> pascal(3, 2)	# Row 4 (1 3 3 1), 3rd entry
+    3
+    """
+    "*** YOUR CODE HERE ***"
+    if row > 4 or column > 4:
+        return 0
+    def sex(current_row = [1]):
+        next_row = [1]
+        if len(current_row) == row+1:
+            return current_row[column]
+        for i in range(len(current_row) - 1):
+            next_row.append(current_row[i]+current_row[i+1])
+        next_row.append(1)
+        return sex(next_row)
+    return sex()
+        
+
+
+
+def compose1(f, g):
+    """"Return a function h, such that h(x) = f(g(x))."""
+    def h(x):
+        return f(g(x))
+    return h
+
+def repeated(f, n):
+    """Return the function that computes the nth application of func (recursively!).
+
+    >>> add_three = repeated(lambda x: x + 1, 3)
+    >>> add_three(5)
+    8
+    >>> square = lambda x: x ** 2
+    >>> repeated(square, 2)(5) # square(square(5))
+    625
+    >>> repeated(square, 4)(5) # square(square(square(square(5))))
+    152587890625
+    >>> repeated(square, 0)(5)
+    5
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(HW_SOURCE_FILE, 'repeated',
+    ...       ['For', 'While'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    #禁止我使用for和while循环是吧。。。。
+    if n == 1:
+        return f
+    if n == 0:
+        g = lambda x:x
+        return compose1(g,g)
+    return compose1(f,repeated(f,n-1))
+
+def num_eights(x):
+    """Returns the number of times 8 appears as a digit of x.
+
+    >>> num_eights(3)
+    0
+    >>> num_eights(8)
+    1
+    >>> num_eights(88888888)
+    8
+    >>> num_eights(2638)
+    1
+    >>> num_eights(86380)
+    2
+    >>> num_eights(12345)
+    0
+    >>> from construct_check import check
+    >>> # ban all assignment statements
+    >>> check(HW_SOURCE_FILE, 'num_eights',
+    ...       ['Assign', 'AugAssign'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    def count_eights(x):
+    # 递归基案：当x为0时，返回0
+        if x == 0:
+            return 0
+        else:
+            # 计算当前位数是否为8，并加上剩余部分的8的数量
+            return (1 if x % 10 == 8 else 0) + count_eights(x // 10)
+    return count_eights(x)
+
+    
+    
+
+
+def pingpong(n):
+    """Return the nth element of the ping-pong sequence.
+
+    >>> pingpong(8)
+    8
+    >>> pingpong(10)
+    6
+    >>> pingpong(15)
+    1
+    >>> pingpong(21)
+    -1
+    >>> pingpong(22)
+    -2
+    >>> pingpong(30)
+    -2
+    >>> pingpong(68)
+    0
+    >>> pingpong(69)
+    -1
+    >>> pingpong(80)
+    0
+    >>> pingpong(81)
+    1
+    >>> pingpong(82)
+    0
+    >>> pingpong(100)
+    -6
+    >>> from construct_check import check
+    >>> # ban assignment statements
+    >>> check(HW_SOURCE_FILE, 'pingpong', ['Assign', 'AugAssign'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    #下面是使用循环和赋值，但是被ban了，以留纪念
+    """
+    res = 0
+    y = 1
+    
+    def switch(y):
+        return 1-y
+    for i in range(1,n+1):
+        if y == 1:
+            res += 1
+        if y == 0:
+            res -= 1
+        if ((i%8) == 0) or num_eights(i):
+            y = switch(y)
+        
+    return res
+    """
+    def sex(index,ppv,diction):
+        if index == n:
+            return ppv
+        else:
+            if index % 8 == 0 or num_eights(index):
+                return sex(index+1,ppv - diction,-diction)
+            else:
+                return sex(index + 1,ppv + diction,diction)
+    return sex(1,1,1)
